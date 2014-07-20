efcacf1fc20dae7fa174b3f79aebf385777237ce
arik-so <arik@sosman.eu>
2014-07-20 17:28:16 +0200

    now checking for new rockets on app launch

--- client/ios/iRonDome/iRonDome/IRDAppDelegate.m
+++ client/ios/iRonDome/iRonDome/IRDAppDelegate.m
@@ -116,6 +116,9 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     application.applicationIconBadgeNumber = 0;
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"newRocket" object:nil]; // whenever the app opens, we ned to check for new rockets
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
